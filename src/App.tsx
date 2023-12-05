891287da043758e6a86d7ade465dc4d53e6a2a0a
MuEbrahim <muhammedebrahim00@gmail.com>
2023-12-05 22:44:37 +0200

    Created the color variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import { Home } from './Route/HomePage/Home';
+import { Home } from './Routes/HomePage/Home';
+
+import './Styles/main.scss'
 
 export function App() {
 	return (
