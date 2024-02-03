16da01b9500147227ef4d12aca936e3718f84489
MuEbrahim <muhammedebrahim00@gmail.com>
2024-02-03 17:54:28 +0200

    Ismail - Adding footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import { Home } from './Routes/HomePage/Home';
 
+import { Footer } from './Components/Footer/Footer';
+
 import './Styles/main.scss'
 
 export function App() {
 	return (
-		<div>
+		<div className='container'>
 			<Router>
 				<Routes>
 					<Route index path='/' element={<Home />} />
 				</Routes>
 			</Router>
+			<Footer />
 		</div>
 	);
 }
