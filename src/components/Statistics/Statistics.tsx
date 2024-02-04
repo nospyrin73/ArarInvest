5247b8e33e5b8dea204d1f64524ac8174257f66a
MuEbrahim <muhammedebrahim00@gmail.com>
2024-02-04 20:01:12 +0200

    Ismail - Adding animation to the statistics component

--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -1,14 +1,33 @@
-import {ReactElement} from "react";
+import {ReactElement, useEffect, useState} from "react";
 
+import Odometer from "react-odometerjs";
+
+import "odometer/themes/odometer-theme-default.css";
 import './Statistics.scss'
 
 type statistic = {
     label: string;
-    value: number | string;
+    value: number;
 }
 
 type StatisticsProps = {
     statistics: statistic[];
+}
+
+type StatisticsOdometerItemProps = {
+    value: number
+}
+
+function StatisticsOdometerItem({value}: StatisticsOdometerItemProps): ReactElement {
+    const [odometerValue, setOdometerValue] = useState(0);
+
+    useEffect(() => {
+        setOdometerValue(value);
+    }, []);
+
+    return (
+        <Odometer format="d" duration={1000} value={odometerValue} />
+    );
 }
 
 export function Statistics({statistics}: StatisticsProps): ReactElement | null {
@@ -22,8 +41,10 @@ export function Statistics({statistics}: StatisticsProps): ReactElement | null {
                 <div className="statistics__box">
                     {
                         statistics.map(({label, value}) => (
-                            <div className="statistics__item">
-                                <span className="statistics__item-value">{value}</span>
+                            <div className="statistics__item" key={`${label}-${value}`}>
+                                <span className="statistics__item-value">
+                                    <StatisticsOdometerItem value={value} />
+                                </span>
                                 <span className="statistics__item-label">{label}</span>
                             </div>
                         ))
